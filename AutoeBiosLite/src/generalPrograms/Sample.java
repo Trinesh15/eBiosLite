406319b5891cf0269817f9e0c18a7802779439f4
DEVARAJA_JINKA <devaraja@biosoftltd.com>
2021-10-27 10:37:21 +0530

    Commit from dev at 10:37

--- AutoeBiosLite/src/generalPrograms/Sample.java
+++ AutoeBiosLite/src/generalPrograms/Sample.java
@@ -8,39 +8,28 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Sample {
 	
 	public static void main(String[] args) {
-//		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-//		WebDriver driver = new ChromeDriver();
-//		driver.manage().window().maximize();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.get("http://localhost:8085/eBiosLite/(S())/eBiosFiles/CommonForms/frmLogin.aspx");
-//		driver.findElement(By.id("inputUser")).sendKeys("bios");
-//		driver.findElement(By.id("inputPassword")).sendKeys("satclt");
-//		driver.findElement(By.id("btnLogIn")).click();
-//		driver.findElement(By.xpath("//i[@class='fa fa-bars']")).click();
-//		driver.findElement(By.xpath("//a[text()='Registration']")).click();
-////		//System.out.println(driver.findElement(By.id("cpBody_txtHospIdNo"))
-////		JavascriptExecutor js = (JavascriptExecutor)driver;
-////		Object a = js.executeScript("document.getElementById('cpBody_txtHospIdNo').value");
-////		System.out.println(a.toString());
-////		System.out.println(driver.findElement(By.id("cpBody_txtHospIdNo")).getAttribute("value"));
-//		String url = driver.getCurrentUrl();
-//		driver.get(url);
-//		driver.findElement(By.id("inputUser")).sendKeys("bios");
-//		driver.findElement(By.id("inputPassword")).sendKeys("satclt");
-//		driver.findElement(By.id("btnLogIn")).click();
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://localhost:8085/eBiosLite/(S())/eBiosFiles/CommonForms/frmLogin.aspx");
+		driver.findElement(By.id("inputUser")).sendKeys("nem");
+		driver.findElement(By.id("inputPassword")).sendKeys("nest");
+		driver.findElement(By.id("btnLogIn")).click();
+		driver.findElement(By.xpath("//i[@class='fa fa-bars']")).click();
+		driver.findElement(By.xpath("//a[text()='Appointments']")).click();
+		Actions act = new Actions(driver);
+		WebElement ele = driver.findElement(By.xpath("(//th[@class='fc-agenda-axis fc-widget-header' and text()='10:00'])[1]"));
+		act.doubleClick(ele).perform();
 		
-		String s= "45.00 ()";
-		String[] ary = s.split(" ");
-		for(String s1: ary) {
-			System.out.println(s1);
-			System.out.println();
-		}
+		
 		
 	}
 }
